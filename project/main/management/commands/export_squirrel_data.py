69c15a67597f8635975988f390ab0bddf37394b6
Kristen Maynor <km3244@columbia.edu>
2021-04-01 02:31:57 +0000

    Export command that can be used to export the data in CSV format

---
+++ project/main/management/commands/export_squirrel_data.py
@@ -0,0 +1,43 @@
+from django.core.management.base import BaseCommand, CommandError
+
+from django.apps import apps
+# from django.db.models import sqdata
+
+import csv
+
+class Command(BaseCommand):
+    help = 'Exports csv data from the given path'
+    
+    def add_arguments(self, parser):
+        parser.add_argument('path', type = str, help = 'Path of the csv file to write')    
+
+    def handle(self, *args, **kwargs ):
+        path = kwargs['path']
+        
+        _model = apps.get_model('main','sqdata')
+        
+        fields = _model._meta.get_fields() #retrieve column names and their types
+        field_names = []
+        for field in fields:
+            field_names.append(field.attname)
+
+        with open(path, mode ='w') as csv_file:
+            writer = csv.writer(csv_file, delimiter=',', quotechar='"')
+            writer.writerow(field_names)
+            
+            for item in _model.objects.all():
+                row_values = []
+                for field_name in field_names:
+                    value = getattr(item, field_name)
+                    field = _model._meta.get_field(field_name)
+                    #Boolean fields print as 0 or 1; 
+                    #This converts to true/false string instead
+                    if field.get_internal_type() == "BooleanField": 
+                        if value == "0":
+                            row_values.append("FALSE")
+                        else:
+                            row_values.append("TRUE")
+                    else:
+                        row_values.append(str(value))
+                
+                writer.writerow(row_values)
